66612b0ff03d2c627d0046803ecf53018be47f6e
shankar87dayal <shankar.87.dayal@gmail.com>
2022-08-29 10:46:01 +0530

    Master Page concept using React _ Blogging Application using

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import Base from './components/Base';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Base>
+    <h1>this is the base components</h1>
+    
+    </Base>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import Base from "../components/Base";
+
+const About = () =>{
+    return (
+        <Base>
+        <h1>This is about page</h1>
+        <p>I am building blog website</p>
+        </Base>
+    )
+}
+export default About;
